e7251ab516e8071fbdcf7ca72d08b0a8795f976a
ryntab <soccershar18@gmail.com>
2024-06-24 17:01:41 -0400

    feat: :construction: add basic view

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import lighthouseRoutes from './routes/lighthouse.js';
 import auditRoutes from './routes/auditRoutes.js';
+import ejs from 'ejs';
+
+import { getDirname } from './utils/getDirname.js';
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = getDirname(import.meta.url);
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/audits', express.static(path.join(__dirname, 'audits')));
 
 // Use the lighthouse routes for starting audits
 app.use('/api/', lighthouseRoutes);
@@ -24,7 +37,12 @@ const test_hook_port = 3001;
 test_hook_server.use(express.json());
 
 test_hook_server.post('/pingback', (req, res) => {
-    console.log('Pingback received:', req.body);
+    const body = req.body;
+    const { data } = body;
+    const { response } = data;
+    const { seo } = response;
+
+    console.log(response)
     res.send({ success: true });
 });
 
@@ -32,6 +50,28 @@ test_hook_server.listen(test_hook_port, () => {
     console.log(`Test Hook Server Running on port: ${test_hook_port} 🚀`);
 });
 
-
+app.get('/view-audits', (req, res) => {
+    const auditsPath = path.join(__dirname, '/', 'audits')
+    console.log(auditsPath)
+    fs.readdir(auditsPath, (err, sites) => {
+        if (err) {
+          return res.status(500).send('Error reading audits directory');
+        }
+    
+        const audits = sites.map(site => {
+          const sitePath = path.join(auditsPath, site);
+          const auditDirs = fs.readdirSync(sitePath).filter(file => fs.lstatSync(path.join(sitePath, file)).isDirectory());
+          const reports = auditDirs.map(auditId => {
+            const reportsPath = path.join(sitePath, auditId, 'reports');
+            const reportFiles = fs.existsSync(reportsPath) ? fs.readdirSync(reportsPath).filter(file => file.endsWith('.html')) : [];
+            return { auditId, reportFiles };
+          });
+    
+          return { site, reports };
+        });
+    
+        res.render('viewAudits', { audits });
+      });
+});
 
 
